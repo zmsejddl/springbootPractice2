9deb827a60b8525fe2b5be91fcb25e0c31c29749
Jung Young Hun <hujung0268@gamil.com>
2024-09-07 13:36:27 +0900

    롬복으로 생성자 만들기

--- sbb2/src/main/java/com/mysite/sbb2/HelloLombok.java
+++ sbb2/src/main/java/com/mysite/sbb2/HelloLombok.java
@@ -1,19 +1,16 @@
 package com.mysite.sbb2;
 
 import lombok.Getter;
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Getter
-@Setter
 public class HelloLombok {
-	private String hello;
-	private int lombok;
+	private final String hello;
+	private final int lombok;
 
 	public static void main(String[] args) {
-		HelloLombok helloLombok = new HelloLombok();
-		helloLombok.setHello("헬로");
-		helloLombok.setLombok(5);
-
+		HelloLombok helloLombok = new HelloLombok("헬로", 5);
 		System.out.println(helloLombok.getHello());
 		System.out.println(helloLombok.getLombok());
 	}
