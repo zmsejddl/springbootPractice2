448d07c84ea74dc87d128326dd91cf7b41181dd3
Jung Young Hun <hujung0268@gamil.com>
2024-09-08 12:58:01 +0900

    질문 엔티티 만들기

---
+++ sbb2/src/main/java/com/mysite/sbb2/Question.java
@@ -0,0 +1,28 @@
+package com.mysite.sbb2;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(length = 200)
+	private String subject;
+
+	@Column(columnDefinition = "TEXT")
+	private String content;
+
+	private LocalDateTime createDate;
+}
