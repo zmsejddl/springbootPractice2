05fe8efdab43a430271c7a5d85ef66fae17d5776
Jung Young Hun <hujung0268@gamil.com>
2024-09-13 11:15:38 +0900

    findBySubject 메서드

--- sbb2/src/test/java/com/mysite/sbb2/Sbb2ApplicationTests.java
+++ sbb2/src/test/java/com/mysite/sbb2/Sbb2ApplicationTests.java
@@ -1,6 +1,10 @@
 package com.mysite.sbb2;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,17 +18,8 @@ class Sbb2ApplicationTests {
 
 	@Test
 	void testJpa() {
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);
-
-		Question q2 = new Question();
-		q2.setSubject("스프링 부트 모델 질문입니다.");
-		q2.setContent("id는 자동으로 생성되나요?");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);
+		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+		assertEquals(1, q.getId());
 	}
 
 }
