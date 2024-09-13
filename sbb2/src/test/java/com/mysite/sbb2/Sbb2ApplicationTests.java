e80f9ddc0ee3c6924d640185f774f19806390c71
Jung Young Hun <hujung0268@gamil.com>
2024-09-13 11:22:44 +0900

    findBySubjectLike 메서드

--- sbb2/src/test/java/com/mysite/sbb2/Sbb2ApplicationTests.java
+++ sbb2/src/test/java/com/mysite/sbb2/Sbb2ApplicationTests.java
@@ -18,8 +18,9 @@ class Sbb2ApplicationTests {
 
 	@Test
 	void testJpa() {
-		Question q = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
-		assertEquals(1, q.getId());
+		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+		Question q = qList.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
 	}
 
 }
