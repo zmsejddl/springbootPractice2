6628f91fd8266c9a0d35c98556226980ea5b34b4
Jung Young Hun <hujung0268@gamil.com>
2024-09-13 11:34:02 +0900

    답변 데이터를 통해 질문 데이터 찾기 VS 질문 데이터를 통해 답변 데이터 찾기

--- sbb2/src/test/java/com/mysite/sbb2/Sbb2ApplicationTests.java
+++ sbb2/src/test/java/com/mysite/sbb2/Sbb2ApplicationTests.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class Sbb2ApplicationTests {
@@ -20,12 +21,17 @@ class Sbb2ApplicationTests {
 	@Autowired
 	private AnswerRepository answerRepository;
 
+	@Transactional
 	@Test
 	void testJpa() {
-		Optional<Answer> oa = this.answerRepository.findById(1);
-		assertTrue(oa.isPresent());
-		Answer a = oa.get();
-		assertEquals(2, a.getQuestion().getId());
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+
+		List<Answer> answerList = q.getAnswerList();
+
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다", answerList.get(0).getContent());
 	}
 
 }
