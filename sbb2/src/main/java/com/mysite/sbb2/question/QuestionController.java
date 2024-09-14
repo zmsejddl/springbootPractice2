1b88f386e526da81daa44a2d9839f99ea8c91d8a
Jung Young Hun <hujung0268@gamil.com>
2024-09-14 13:04:07 +0900

    상세 페이지 컨트롤러 만들기

--- sbb2/src/main/java/com/mysite/sbb2/question/QuestionController.java
+++ sbb2/src/main/java/com/mysite/sbb2/question/QuestionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,5 +20,10 @@ public class QuestionController {
 		List<Question> questionList = this.questionService.getList();
 		model.addAttribute("questionList", questionList);
 		return "question_list";
+	}
+
+	@GetMapping(value = "/question/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id) {
+		return "question_detail";
 	}
 }
