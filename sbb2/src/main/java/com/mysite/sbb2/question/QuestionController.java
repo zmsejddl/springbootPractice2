4d5f0ea6dcde35b9ea3d45ad44ee09848aca55b3
Jung Young Hun <hujung0268@gamil.com>
2024-09-14 13:09:40 +0900

    상세 페이지에 서비스 사용하기

--- sbb2/src/main/java/com/mysite/sbb2/question/QuestionController.java
+++ sbb2/src/main/java/com/mysite/sbb2/question/QuestionController.java
@@ -24,6 +24,8 @@ public class QuestionController {
 
 	@GetMapping(value = "/question/detail/{id}")
 	public String detail(Model model, @PathVariable("id") Integer id) {
+		Question question = this.questionService.getQuestion(id);
+		model.addAttribute("question", question);
 		return "question_detail";
 	}
 }
