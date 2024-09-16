71b4dcc5844c2908b0ffae43917ce8b5f48e155b
Jung Young Hun <hujung0268@gamil.com>
2024-09-16 12:03:46 +0900

    답변 서비스 만들기

--- sbb2/src/main/java/com/mysite/sbb2/answer/AnswerController.java
+++ sbb2/src/main/java/com/mysite/sbb2/answer/AnswerController.java
@@ -18,12 +18,13 @@ import lombok.RequiredArgsConstructor;
 public class AnswerController {
 
 	private final QuestionService questionService;
+	private final AnswerService answerService;
 
 	@PostMapping("/create/{id}")
 	public String createAnswer(Model model, @PathVariable("id") Integer id,
 			@RequestParam(value = "content") String content) {
 		Question question = this.questionService.getQuestion(id);
-		// TODO: 답변을 저장한다.
+		this.answerService.create(question, content);
 		return String.format("redirect:/question/detail/%s", id);
 	}
 }
