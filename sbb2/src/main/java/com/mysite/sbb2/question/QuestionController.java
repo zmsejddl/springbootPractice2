a6c15fa5cd56f4e961cba40c22dd1166fb6165fe
Jung Young Hun <hujung0268@gamil.com>
2024-09-17 13:16:45 +0900

    답변 등록 기능에 폼 적용하기

--- sbb2/src/main/java/com/mysite/sbb2/question/QuestionController.java
+++ sbb2/src/main/java/com/mysite/sbb2/question/QuestionController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.mysite.sbb2.answer.AnswerForm;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -29,7 +31,7 @@ public class QuestionController {
 	}
 
 	@GetMapping(value = "/detail/{id}")
-	public String detail(Model model, @PathVariable("id") Integer id) {
+	public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
 		Question question = this.questionService.getQuestion(id);
 		model.addAttribute("question", question);
 		return "question_detail";
