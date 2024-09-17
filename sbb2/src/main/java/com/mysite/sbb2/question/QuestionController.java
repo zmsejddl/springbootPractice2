34f8f56c49dcd7e23dbcdcd9f7877b6ff5f8b00f
Jung Young Hun <hujung0268@gamil.com>
2024-09-17 13:02:48 +0900

    질문 등록 버튼과 화면 만들기

--- sbb2/src/main/java/com/mysite/sbb2/question/QuestionController.java
+++ sbb2/src/main/java/com/mysite/sbb2/question/QuestionController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 
@@ -29,5 +31,17 @@ public class QuestionController {
 		Question question = this.questionService.getQuestion(id);
 		model.addAttribute("question", question);
 		return "question_detail";
+	}
+
+	@GetMapping("/create")
+	public String questionCreate() {
+		return "question_form";
+	}
+
+	@PostMapping("/create")
+	public String questionCreate(@RequestParam(value = "subject") String subject,
+			@RequestParam(value = "content") String content) {
+		this.questionService.create(subject, content);
+		return "redirect:/question/list"; // 질문 저장 후 질문 목록으로 이동
 	}
 }
