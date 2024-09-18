d66fe4876cb4a49c3a47577d454116978f47b34b
Jung Young Hun <hujung0268@gamil.com>
2024-09-18 12:35:41 +0900

    최신순으로 데이터 조회하기

--- sbb2/src/main/java/com/mysite/sbb2/question/QuestionService.java
+++ sbb2/src/main/java/com/mysite/sbb2/question/QuestionService.java
@@ -1,12 +1,14 @@
 package com.mysite.sbb2.question;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.mysite.sbb2.DataNotFoundException;
@@ -20,7 +22,9 @@ public class QuestionService {
 	private final QuestionRepository questionRepository;
 
 	public Page<Question> getList(int page) {
-		Pageable pageable = PageRequest.of(page, 10);
+		List<Sort.Order> sorts = new ArrayList<>();
+		sorts.add(Sort.Order.desc("createDate"));
+		Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
 		return this.questionRepository.findAll(pageable);
 	}
 
