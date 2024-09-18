fa7b31c8a29966d095cf991e799e1befd4839416
Jung Young Hun <hujung0268@gamil.com>
2024-09-18 12:21:25 +0900

    대량 테스트 데이터 만들기

--- sbb2/src/test/java/com/mysite/sbb2/Sbb2ApplicationTests.java
+++ sbb2/src/test/java/com/mysite/sbb2/Sbb2ApplicationTests.java
@@ -16,27 +16,21 @@ import com.mysite.sbb2.answer.Answer;
 import com.mysite.sbb2.answer.AnswerRepository;
 import com.mysite.sbb2.question.Question;
 import com.mysite.sbb2.question.QuestionRepository;
+import com.mysite.sbb2.question.QuestionService;
 
 @SpringBootTest
 class Sbb2ApplicationTests {
 
 	@Autowired
-	private QuestionRepository questionRepository;
+	private QuestionService questionService;
 
-	@Autowired
-	private AnswerRepository answerRepository;
-
-	@Transactional
 	@Test
 	void testJpa() {
-		Optional<Question> oq = this.questionRepository.findById(2);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
-
-		List<Answer> answerList = q.getAnswerList();
-
-		assertEquals(1, answerList.size());
-		assertEquals("네 자동으로 생성됩니다", answerList.get(0).getContent());
+		for (int i = 1; i <= 300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String content = "내용 없음";
+			this.questionService.create(subject, content);
+		}
 	}
 
 }
