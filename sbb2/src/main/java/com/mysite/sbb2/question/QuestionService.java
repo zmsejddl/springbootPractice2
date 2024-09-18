e77245a6b57197e35f27c47ffb7e2680f3e1d449
Jung Young Hun <hujung0268@gamil.com>
2024-09-18 12:24:46 +0900

    페이징 구현하기

--- sbb2/src/main/java/com/mysite/sbb2/question/QuestionService.java
+++ sbb2/src/main/java/com/mysite/sbb2/question/QuestionService.java
@@ -4,6 +4,9 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.mysite.sbb2.DataNotFoundException;
@@ -15,6 +18,11 @@ import lombok.RequiredArgsConstructor;
 public class QuestionService {
 
 	private final QuestionRepository questionRepository;
+
+	public Page<Question> getList(int page) {
+		Pageable pageable = PageRequest.of(page, 10);
+		return this.questionRepository.findAll(pageable);
+	}
 
 	public List<Question> getList() {
 		return this.questionRepository.findAll();
