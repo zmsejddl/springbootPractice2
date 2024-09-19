cf11a31a0364f8560c6b5d75263c519a963cd2a3
Jung Young Hun <hujung0268@gamil.com>
2024-09-19 11:54:57 +0900

    로그아웃 기능 구현하기

--- sbb2/src/main/java/com/mysite/sbb2/SecurityConfig.java
+++ sbb2/src/main/java/com/mysite/sbb2/SecurityConfig.java
@@ -22,7 +22,9 @@ public class SecurityConfig {
 				.csrf((csrf) -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
 				.headers((headers) -> headers.addHeaderWriter(
 						new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
-				.formLogin((formLogin) -> formLogin.loginPage("/user/login").defaultSuccessUrl("/"));
+				.formLogin((formLogin) -> formLogin.loginPage("/user/login").defaultSuccessUrl("/"))
+				.logout((logout) -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+						.logoutSuccessUrl("/").invalidateHttpSession(true));
 		return http.build();
 	}
 
