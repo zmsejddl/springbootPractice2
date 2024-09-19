d2284d51f50d67bcc9201af4e19d11bd036ca574
Jung Young Hun <hujung0268@gamil.com>
2024-09-19 11:35:46 +0900

    중복 회원 가입 방지하기

--- sbb2/src/main/java/com/mysite/sbb2/user/UserController.java
+++ sbb2/src/main/java/com/mysite/sbb2/user/UserController.java
@@ -1,5 +1,6 @@
 package com.mysite.sbb2.user;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,7 +33,17 @@ public class UserController {
 			return "signup_form";
 		}
 
-		userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword1());
+		try {
+			userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword1());
+		} catch (DataIntegrityViolationException e) {
+			e.printStackTrace();
+			bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+			return "signup_form";
+		} catch (Exception e) {
+			e.printStackTrace();
+			bindingResult.reject("signupFailed", e.getMessage());
+			return "signup_form";
+		}
 
 		return "redirect:/";
 	}
