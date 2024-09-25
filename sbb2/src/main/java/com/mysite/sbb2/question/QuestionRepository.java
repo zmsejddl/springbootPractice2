fe50190670abe091af79c6b6ca7dca2387654351
Jung Young Hun <hujung0268@gamil.com>
2024-09-25 13:20:06 +0900

    별거 안 바꿈

--- sbb2/src/main/java/com/mysite/sbb2/question/QuestionRepository.java
+++ sbb2/src/main/java/com/mysite/sbb2/question/QuestionRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
 	Question findBySubject(String subject);
